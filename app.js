597808a1ef61ce2998c4ddd28009b4fa0a0db6d4
Marian-Claudiu Culea <marianclaudiu.culea@gmail.com>
2021-12-02 17:05:08 +0100

    Muie

--- app.js
+++ app.js
@@ -1,8 +1,4 @@
 const mongoose= require('mongoose');
-//const db= require('./models');
-//express app
-//connect to mongodb
-
 
 const express=require('express');
 const rootRouter = require('./routes/index');
@@ -10,10 +6,6 @@ const rootRouter = require('./routes/index');
 const app=express();
 app.use(express.json());
 app.use(rootRouter);
-
-//listen for request
-// const port = process.env.PORT || 3000;
-// app.listen(port, () => console.log( 'Listening on port ${port}...' ));
 
 const dbURI='mongodb+srv://user1:123abc@bpr.tahew.mongodb.net/BPR_Atlas?retryWrites=true&w=majority'
 mongoose.connect(dbURI,{useNewUrlParser:true,useUnifiedTopology:true})
