1343d93b919ed510c6bbf454da3c60b8d4ab6f64
Marian-Claudiu Culea <marianclaudiu.culea@gmail.com>
2021-12-02 17:29:52 +0100

    test

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ const express=require('express');
 const rootRouter = require('./routes/index');
 
 const app=express();
-app.use(express.json());
-app.use(rootRouter);
+app.use(express.json());!
+app.use(rootRouter);{
 
 const dbURI='mongodb+srv://user1:123abc@bpr.tahew.mongodb.net/BPR_Atlas?retryWrites=true&w=majority'
 mongoose.connect(dbURI,{useNewUrlParser:true,useUnifiedTopology:true})
